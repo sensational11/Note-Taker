443546a819257fe6c92c25c29c66957135de2740
sensational.mack <mackn478@gmail.com>
2022-11-13 13:40:44 -0500

    updated routes

--- apiroute/myRoutehtml.js
+++ apiroute/myRoutehtml.js
@@ -1,11 +1,13 @@
-// const path = require("path");
-// const router = require("express").Router();
+const router = require("express").Router();
+const path = require("path");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(_dirname, "../public/notes.html"));
+});
+router.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "../public/index.html"));
+});
 
-// router.get("/notes", (req,res)=>{
-//     res.sendFile(path.join(_dirname, "../public/notes.html"));
-// });
-// router.get("*", (req,res)=>{
-//     res.sendFile(path.join(_dirname, "../public/index.html")
-// })
-
-// module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require("express");
 const path = require("path");
 
 
-// const apiRoutes = require("./apiroute/myApiRoute");
+const apiRoutes = require("./apiroute/myApiRoute");
 
 //this is calling express
 const app = express();
-// const htmlRoutes = require("./apiroute/myRoutehtml");
+const htmlRoutes = require("./apiroute/myRoutehtml");
 
 //this is setting the port for the server to use.
 const PORT = process.env.PORT || 3302;
@@ -16,16 +16,16 @@ const PORT = process.env.PORT || 3302;
 //The app.use() method notifies the express library that our front end code is houses in the Public Folder. It's going to the entry point the index file. 
 app.use(express.static("public"));
 
-// app.use(express.urlencoded({extended: true }));
-// app.use(express.json());
-// app.use("/", htmlRoutes);
-// app.use("/", apiRoutes);
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
 // app.get("/", (req, res) => res.send("this is the routes"));
 //This is sending the server information to the client. The first paramater we pass in is the url then the message we want to display. Req is requirement and res is the response. Sending the file informtion to the client. The path.join is allow us to use the express path comonenet and the __dirname is how it recognizes the parent folder we're calling from. Then we set our path. 
-app.get("/public", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"))
-});
+// app.get("/public", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/index.html"))
+// });
 
 
 
