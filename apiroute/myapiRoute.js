a3a10b50e5a989d8774ce9d9ad460dc46832ab36
sensational.mack <mackn478@gmail.com>
2022-11-13 14:00:57 -0500

    upated routes

--- apiroute/myapiRoute.js
+++ apiroute/myapiRoute.js
@@ -1,4 +1,3 @@
-const express = require("express").Router
 const path = require("path");
 const db = require("../db/db.json");
 const router = require("express").Router();
@@ -6,6 +5,20 @@ const router = require("express").Router();
 const fs = require("fs");
 const uuid = require("uuid");
 
-router.get("./notes", (req,res)=>{
-    fs.readFile("../db.json").then((data)=>res.json(JSON.parse))
-})
+router.get("/notes", (req, res) => {
+  let results = JSON.parse(fs.readFileSync("./db/db.json"));
+  res.send(results)
+});
+
+router.post("/notes", (req,res) => {
+    let notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    notes.push({
+        title: req.body.title,
+        text: req.body.text,
+        id: `${notes.length}`,
+    });
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+module.exports= router;
