d55ad1aa3c1ec3688d6aa8e575a79707c44a8359
sensational.mack <mackn478@gmail.com>
2022-11-14 07:38:25 -0500

    update apiroute

--- apiroute/myapiRoute.js
+++ apiroute/myapiRoute.js
@@ -7,18 +7,29 @@ const uuid = require("uuid");
 
 router.get("/notes", (req, res) => {
   let results = JSON.parse(fs.readFileSync("./db/db.json"));
-  res.send(results)
+  res.send(results);
 });
 
-router.post("/notes", (req,res) => {
-    let notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    notes.push({
-        title: req.body.title,
-        text: req.body.text,
-        id: `${notes.length}`,
-    });
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes);
+router.post("/notes", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  notes.push({
+    title: req.body.title,
+    text: req.body.text,
+    id: `${notes.length}`,
+  });
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
 });
 
-module.exports= router;
+router.delete("notes/:id", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./data/notes.json"));
+  const notesIndex = notes.findIndex((note) => note.id === req.params.id);
+  if (notesIndex === -1) return res.status(404).json({});
+
+  notes.splice(notesIndex, -1);
+  fs.writeFileSync("./data/notes.json", JSON.stringify(notes));
+
+  res.json(notes);
+});
+
+module.exports = router;
